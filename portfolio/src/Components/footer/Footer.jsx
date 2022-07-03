2fb41383b4194b7b4b651d8f946f6616921b646a
Shahbazhassan42000 <Shahbazhassan42000@gmail.com>
2022-07-03 16:17:30 +0500

    fix bottom leaves height

--- portfolio/src/Components/footer/Footer.jsx
+++ portfolio/src/Components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import leaves from "../../assets/images/hero-design.svg";
 const Footer = () => (
     <div id="footer" className="footer relative px-[12%] flex flex-col mt-[151px]">
         <FooterNav/>
-        <img className="hidden sm:block absolute left-[-67px] top-[-67px] h-[380px]" src={leaves} alt={'leaves picture'}/>
+        <img className="hidden sm:block absolute left-[-67px] top-[-75px] h-[380px]" src={leaves} alt={'leaves picture'}/>
         <p className="self-center text-[0.6rem] sm:text-[16px] text-white mb-[20px] mt-[90px] sm:mt-[150px]">Copyright © 2021 Maham Zahid All Rights Reserved</p>
     </div>
 )
